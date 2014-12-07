4420d75ae421bfee4122bad4cdc75e23b48ba4b0
Sarah Pearson <sarahp@me.com>
2014-12-07 09:25:10 -0600

    styling to note show page

--- client/views/notes/notes.js
+++ client/views/notes/notes.js
@@ -2,8 +2,10 @@
   'use strict';
 
   angular.module('hapi-auth')
-  .controller('NotesCtrl', ['$rootScope', '$scope', '$state', 'Note', function($rootScope, $scope, $state, Note){
+  .controller('NotesCtrl', ['$rootScope', '$scope', '$state', 'Note', '$stateParams', function($rootScope, $scope, $state, Note, $stateParams){
     $scope.note = {};
+    $scope.mode = $state.current.name;
+
 
     function getRecent(){
       Note.recent().then(function(response){
@@ -12,6 +14,24 @@
     }
 
     getRecent();
+
+    // if($scope.mode !== 'viewNote'){
+    //   Note.getRecent().then(function(response){
+    //     $scope.notes = response.data;
+    //   });
+    // }
+
+    if($scope.mode === 'viewNote'){
+      var noteId = $stateParams.noteId;
+      console.log('NOTEID is', noteId);
+      Note.findOne(noteId).then(function(response){
+        debugger;
+        $scope.note = response.data[0];
+      }, function(response){
+        //reject promise
+        console.log('promise rejected', response);
+      });
+    }
 
     $scope.create = function(note){
       Note.create(note).then(function(response){
