97a55bebaef3d6c6ff26535871ef8228ab38427b
Sarah Pearson <sarahp@me.com>
2014-12-09 13:50:27 -0600

    notes jade updated

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var Hapi         = require('hapi'),
-  server         = new Hapi.Server('0.0.0.0', process.env.PORT),
+  server         = new Hapi.Server('0.0.0.0', process.env.PORT, {cors:true}), //cors:true is telling hapi it is okay if someone talks to you from a different domain
   routes         = require('./routes/config/routes'),
   plugins        = require('./routes/config/plugins'),
   authentication = require('./routes/config/authentication');
